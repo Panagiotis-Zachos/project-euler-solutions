1ccf569f048a3a3068cd42629e2c770cf3627bda
Panagiotis Zachos <panzaxos@gmail.com>
2022-07-27 14:12:18 +0300

    Create maximum_path_sum_1

---
+++ maximum_path_sum_1.py
@@ -0,0 +1,11 @@
+
+if __name__ == "__main__":
+
+    with open('.\input_files\maximum_path_sum_1.txt') as f:
+        num_triangle = [list(map(int, x.split())) for x in f.read().split('\n')]
+
+        for i in range(len(num_triangle)-1,0,-1):
+            for j in range(len(num_triangle[i])-1):
+                num_triangle[i-1][j] += max(num_triangle[i][j:j+2])
+
+        print(num_triangle[0])
