b0228a4a4369ee6661228c59c525f0d216921e37
Panagiotis Zachos <panzaxos@gmail.com>
2022-08-18 16:17:17 +0300

    Create 1000-digit_Fibonacci_number.py

---
+++ 1000-digit_Fibonacci_number.py
@@ -0,0 +1,16 @@
+from cmath import log
+from math import log10, sqrt
+
+if __name__ == "__main__":
+    """
+    Find the index of the first fibonacci number with more than 1000 digits.
+    This is based upon the fibonacci number approximation function:
+    F(n) = PHI ** n / sqrt(5)
+    and the fact that by taking the int(log10(F)) + 1 we can easily count the digits of the result
+    """
+    PHI = (1 + 5 ** 0.5) / 2
+
+    fib_num = 1
+    while int(fib_num * log10(PHI) - log10(sqrt(5)))+1 < 1000:
+        fib_num += 1
+    print(fib_num)
