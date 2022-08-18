a25216030e4a856d1b34e3fb8ab5a3c407b32098
Panagiotis Zachos <panzaxos@gmail.com>
2022-08-18 15:50:30 +0300

    Added time to complete as comment

--- lexicographic_permutations.py
+++ lexicographic_permutations.py
@@ -4,6 +4,7 @@ import itertools
 if __name__ == "__main__":
     """
     Definetely not the most efficent solution, but I couldn't resist the 1-liner solution this time
+    9.3 seconds on my i5-10gen XPS13
     """
     t0 = time()
     perms = [''.join(map(str, i)) for i in itertools.permutations([0,1,2,3,4,5,6,7,8,9], 10)]
