a75ed17fe5b0c07505d306caf5e06035a2845b79
Panagiotis Zachos <panzaxos@gmail.com>
2022-08-18 16:19:09 +0300

    Update 1000-digit_Fibonacci_number.py

--- 1000-digit_Fibonacci_number.py
+++ 1000-digit_Fibonacci_number.py
@@ -1,5 +1,6 @@
-from cmath import log
 from math import log10, sqrt
+from time import time
+
 
 if __name__ == "__main__":
     """
@@ -7,10 +8,13 @@ if __name__ == "__main__":
     This is based upon the fibonacci number approximation function:
     F(n) = PHI ** n / sqrt(5)
     and the fact that by taking the int(log10(F)) + 1 we can easily count the digits of the result
+
+    Completes in 0.0159 seconds on i5-10gen Dell XPS13
     """
     PHI = (1 + 5 ** 0.5) / 2
-
+    t0 = time()
     fib_num = 1
     while int(fib_num * log10(PHI) - log10(sqrt(5)))+1 < 1000:
         fib_num += 1
     print(fib_num)
+    print('Time to complete: ' + str(time() - t0))
